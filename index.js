d9bd2007fc70f158c35aae79fbeba784fa65f6c5
Akhand2021 <apsraghuvanshi85@gmail.com>
2023-07-14 11:16:31 +0530

    changes

--- index.js
+++ index.js
@@ -2,31 +2,41 @@ const promptInput = document.getElementById("promptInput");
 const generateBtn = document.getElementById("generateBtn");
 const resultText = document.getElementById("resultText");
 
-const handleEnterKey = (event) => {
-  if (event.key === "Enter") {
-    generateBtn.disabled = true;
-    generateBtn.innerText = "Generating...";
+// Function to handle the API request
+const handleGenerate = () => {
+  // Disable the "Generate" button
+  generateBtn.disabled = true;
+  generateBtn.innerText = "Generating...";
 
-    fetch('generate.php', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      body: 'promptInput=' + encodeURIComponent(promptInput.value)
+  fetch('generate.php', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: 'promptInput=' + encodeURIComponent(promptInput.value)
+  })
+    .then(response => response.text())
+    .then(data => {
+      resultText.innerText = data;
+      // Enable the "Generate" button
+      generateBtn.disabled = false;
+      generateBtn.innerText = "Generate";
     })
-      .then(response => response.text())
-      .then(data => {
-        resultText.innerText = data;
-        generateBtn.disabled = false;
-        generateBtn.innerText = "Generate";
-      })
-      .catch(error => {
-        console.error('Error:', error);
-        resultText.innerText = 'Error occurred while generating.';
-        generateBtn.disabled = false;
-        generateBtn.innerText = "Generate";
-      });
-  }
+    .catch(error => {
+      console.error('Error:', error);
+      resultText.innerText = 'Error occurred while generating.';
+      // Enable the "Generate" button
+      generateBtn.disabled = false;
+      generateBtn.innerText = "Generate";
+    });
 };
 
-promptInput.addEventListener("keyup", handleEnterKey);
+// Trigger the API request on Enter key press
+promptInput.addEventListener("keyup", (event) => {
+  if (event.key === "Enter") {
+    handleGenerate();
+  }
+});
+
+// Trigger the API request on "Generate" button click
+generateBtn.addEventListener("click", handleGenerate);
